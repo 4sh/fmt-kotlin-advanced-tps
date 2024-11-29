a8f9d889721d5469eb28b594feab5970b6fc0caa
Morgan Renou <morgan.renou@4sh.fr>
2024-11-29 14:47:13 +0100

    tp1 - step5

---
+++ src/test/kotlin/fmt/kotlin/advanced/FibonacciTest.kt
@@ -0,0 +1,93 @@
+package fmt.kotlin.advanced
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import java.math.BigInteger
+
+class FibonacciTest {
+    fun fibonacci(nthNumber: BigInteger): BigInteger {
+        fun fibonacci(nthNumber: BigInteger): BigInteger {
+            if (nthNumber <= BigInteger.ONE) {
+                return nthNumber
+            }
+
+            return fibonacci(nthNumber - BigInteger.TWO)  + fibonacci(nthNumber - BigInteger.ONE)
+        }
+
+        return fibonacci(nthNumber - BigInteger.ONE)
+    }
+
+    @Test
+    fun `compute first`() {
+        val n = BigInteger.valueOf(1)
+        val fibonacciNumber = fibonacci(n)
+
+        fibonacciNumber.toString().takeLast(10) shouldBe "0"
+        println("The $n-th Fibonacci number is: $fibonacciNumber")
+    }
+
+    @Test
+    fun `compute second`() {
+        val n = BigInteger.valueOf(2)
+        val fibonacciNumber = fibonacci(n)
+
+        fibonacciNumber.toString().takeLast(10) shouldBe "1"
+        println("The $n-th Fibonacci number is: $fibonacciNumber")
+    }
+
+    @Test
+    fun `compute third`() {
+        val n = BigInteger.valueOf(3)
+        val fibonacciNumber = fibonacci(n)
+
+        fibonacciNumber.toString().takeLast(10) shouldBe "1"
+        println("The $n-th Fibonacci number is: $fibonacciNumber")
+    }
+
+
+    @Test
+    fun `compute 4 nth`() {
+        val n = BigInteger.valueOf(4)
+        val fibonacciNumber = fibonacci(n)
+
+        fibonacciNumber.toString().takeLast(10) shouldBe "2"
+        println("The $n-th Fibonacci number is: $fibonacciNumber")
+    }
+
+    @Test
+    fun `compute 5 nth`() {
+        val n = BigInteger.valueOf(5)
+        val fibonacciNumber = fibonacci(n)
+
+        fibonacciNumber.toString().takeLast(10) shouldBe "3"
+        println("The $n-th Fibonacci number is: $fibonacciNumber")
+    }
+
+    @Test
+    fun `compute 6 nth`() {
+        val n = BigInteger.valueOf(6)
+        val fibonacciNumber = fibonacci(n)
+
+        fibonacciNumber.toString().takeLast(10) shouldBe "5"
+        println("The $n-th Fibonacci number is: $fibonacciNumber")
+    }
+
+
+    @Test
+    fun `compute 7 nth`() {
+        val n = BigInteger.valueOf(7)
+        val fibonacciNumber = fibonacci(n)
+
+        fibonacciNumber.toString().takeLast(10) shouldBe "8"
+        println("The $n-th Fibonacci number is: $fibonacciNumber")
+    }
+
+    @Test
+    fun `compute 100 000 nth`() {
+        val n =  BigInteger.valueOf(100_000)
+        val fibonacciNumber = fibonacci(n)
+
+        fibonacciNumber.toString().takeLast(10) shouldBe "6278790626"
+        println("The 10 last digits of the $n-th Fibonacci number is: ${fibonacciNumber.toString().takeLast(10)}")
+    }
+}
