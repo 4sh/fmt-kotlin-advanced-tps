0c5f9ef9c47075037f09ca0b2d1c40d87934f2a1
Morgan Renou <morgan.renou@4sh.fr>
2024-11-29 01:10:31 +0100

    tp2 - step1 - solution

--- src/main/kotlin/fmt/kotlin/advanced/WineRack.kt
+++ src/main/kotlin/fmt/kotlin/advanced/WineRack.kt
@@ -9,29 +9,64 @@ data class Capacity(val nbOfShelves: Int, val maxSlotByShelf: Int)
 // tp2-step1-001
 // hint - extension function
 // hint - operator overload
-data class Position(val shelfIndex: Int, val slotIndex: Int)
+data class Position(val shelfIndex: Int, val slotIndex: Int) : Comparable<Capacity> {
+    override fun compareTo(other: Capacity): Int =
+        when (shelfIndex.compareTo(other.nbOfShelves - 1)) {
+            0, -1 -> slotIndex.compareTo(other.maxSlotByShelf - 1)
+            else -> 1
+        }
+}
+
+// tp2-step1-004
+// hint - extension function
+// hint - operator overload
+infix fun Int.by(maxSlotByShelf: Int) = Capacity(this, maxSlotByShelf)
+
+// tp2-step1-004
+// hint - extension function
+// hint - operator overload
+infix fun Int.at(slotIndex: Int) = Position(this, slotIndex)
+
+// tp2-step1-003
+// hint - typealias
+typealias Mutable2DList<T> = MutableList<MutableList<T>>
 
 
 data class WineRack(val capacity: Capacity, val rackId: String = UUID.randomUUID().toString()) {
 
     // tp2-step1-003
     // hint - typealias
-    private val bottles: MutableList<MutableList<Bottle?>> = mutableListOf<MutableList<Bottle?>>().apply {
+    private val bottles: Mutable2DList<Bottle?> = mutableListOf<MutableList<Bottle?>>().apply {
         repeat(capacity.nbOfShelves) {
-           add(arrayOfNulls<Bottle>(capacity.maxSlotByShelf).toMutableList())
+            add(arrayOfNulls<Bottle>(capacity.maxSlotByShelf).toMutableList())
         }
+    }
+
+    // tp2-step1-002
+    // hint - extension function
+    // hint - operator overload
+    operator fun get(position: Position) = bottles[position]
+
+    // tp2-step1-002
+    // hint - extension function
+    // hint - operator overload
+    operator fun get(shelfIndex: Int): List<Bottle?> = bottles[shelfIndex].toList()
+
+    // tp2-step1-002
+    // hint - extension function
+    // hint - operator overload
+    operator fun set(position: Position, bottle: Bottle) {
+        storeBottle(bottle, position)
     }
 
     // tp2-step1-001
     // hint - extension function
     // hint - operator overload
     fun storeBottle(bottle: Bottle, position: Position) {
-        check(position.shelfIndex < capacity.nbOfShelves && position.slotIndex < capacity.maxSlotByShelf)
-        { "wine rack position $position is out of capacity $capacity" }
-        check(bottles[position.shelfIndex][position.slotIndex] == null)
-        { "the slot at $position is not free" }
+        check(position <= capacity) { "wine rack position $position is out of capacity $capacity" }
+        check(bottles[position] == null) { "the slot at $position is not free" }
 
-        bottles[position.shelfIndex][position.slotIndex] = bottle
+        bottles[position] = bottle
     }
 
     // tp2-step1-003
@@ -39,13 +74,13 @@ data class WineRack(val capacity: Capacity, val rackId: String = UUID.randomUUID
     // hint - operator overload
     fun takeBottle(position: Position): Bottle? =
         viewBottle(position).also {
-            bottles[position.shelfIndex][position.slotIndex] = null
+            bottles[position] = null
         }
 
     // tp2-step1-003
     // hint - extension function
     // hint - operator overload
-    fun viewBottle(position: Position): Bottle? = bottles[position.shelfIndex][position.slotIndex]
+    fun viewBottle(position: Position): Bottle? = bottles[position]
 
     val numberOfBottles: Int
         get() = bottles.sumOf { it.filterNotNull().size }
@@ -59,7 +94,7 @@ data class WineRack(val capacity: Capacity, val rackId: String = UUID.randomUUID
         val maxLengthBySlotIndex: Map<Int, Int> = (0 until capacity.maxSlotByShelf)
             .associateWith { slotIndex ->
                 (0 until capacity.nbOfShelves)
-                    .maxOf { shelfIndex -> (bottles[shelfIndex][slotIndex]?.name ?: EMPTY).length + 5 }
+                    .maxOf { shelfIndex -> (bottles[shelfIndex at slotIndex]?.name ?: EMPTY).length + 5 }
             }
         return bottles.joinToString("\n") { shelf ->
             shelf
@@ -71,18 +106,19 @@ data class WineRack(val capacity: Capacity, val rackId: String = UUID.randomUUID
         } + "\n"
     }
 
-    // tp2-step1-002
-    // hint - extension function
-    // hint - operator overload
-    fun at(shelfIndex: Int, slotIndex: Int): Bottle? {
-        return bottles[shelfIndex][slotIndex]
-    }
 
-    // tp2-step1-002
+    // tp2-step1-003
     // hint - extension function
     // hint - operator overload
-    fun at(shelfIndex: Int): MutableList<Bottle?> {
-        return bottles[shelfIndex]
+    private operator fun Mutable2DList<Bottle?>.get(position: Position) =
+        this[position.shelfIndex][position.slotIndex]
+
+    // tp2-step1-003
+    // hint - extension function
+    // hint - operator overload
+    private operator fun Mutable2DList<Bottle?>.set(position: Position, bottle: Bottle?) {
+        this[position.shelfIndex][position.slotIndex] = bottle
+
     }
 }
 
--- src/test/kotlin/fmt/kotlin/advanced/WineCellarOrganizerTest.kt
+++ src/test/kotlin/fmt/kotlin/advanced/WineCellarOrganizerTest.kt
@@ -9,7 +9,6 @@ import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.maps.shouldContain
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldHave
 import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.types.shouldBeSameInstanceAs
 import org.junit.jupiter.api.Nested
@@ -37,7 +36,7 @@ class WineCellarOrganizerTest {
     @Nested
     inner class `with racks having sufficent space` {
 
-        val wineOrganizer = WineCellarOrganizer(3 to (Capacity(4, 6)))
+        val wineOrganizer = WineCellarOrganizer(3 to (4 by 6))
 
         @Test
         fun `a stored bottle should can be viewed`() {
@@ -542,7 +541,7 @@ class WineCellarOrganizerTest {
     @Nested
     inner class `with racks having unique shelf` {
 
-        val wineOrganizer = WineCellarOrganizer(3 to Capacity(1, 6))
+        val wineOrganizer = WineCellarOrganizer(3 to (1 by 6))
 
         @Nested
         inner class `a common bottle` {
@@ -869,7 +868,7 @@ class WineCellarOrganizerTest {
     @Nested
     inner class `with racks having two shelves` {
 
-        val wineOrganizer = WineCellarOrganizer(3 to Capacity(2, 6))
+        val wineOrganizer = WineCellarOrganizer(3 to (2 by 6))
 
         @Nested
         inner class `a common bottle` {
@@ -1074,7 +1073,7 @@ class WineCellarOrganizerTest {
     @Nested
     inner class `with racks having three shelves` {
 
-        val wineOrganizer = WineCellarOrganizer(3 to Capacity(3, 6))
+        val wineOrganizer = WineCellarOrganizer(3 to (3 by 6))
 
         @Nested
         inner class `a common bottle` {
@@ -1165,7 +1164,7 @@ class WineCellarOrganizerTest {
     @Nested
     inner class `with one rack having sufficent space` {
 
-        val wineOrganizer = WineCellarOrganizer(1 to Capacity(4, 6))
+        val wineOrganizer = WineCellarOrganizer(1 to (4 by 6))
 
         @Test
         fun `stored bottle from regions should can be viewed`() {
@@ -1237,7 +1236,7 @@ class WineCellarOrganizerTest {
     @Nested
     inner class `with two racks having sufficent space` {
 
-        val wineOrganizer = WineCellarOrganizer(2 to Capacity(4, 6))
+        val wineOrganizer = WineCellarOrganizer(2 to (4 by 6))
 
         @Test
         fun `stored bottle from regions should can be viewed`() {
@@ -1419,8 +1418,8 @@ class WineCellarOrganizerTest {
     }
 
     // tp2-step1-002
-    private fun WineRack.getAt(shelfIndex: Int, slotIndex: Int) = at(shelfIndex, slotIndex)
-    private fun WineRack.getAt(shelfIndex: Int) = at(shelfIndex)
+    private fun WineRack.getAt(shelfIndex: Int, slotIndex: Int) = this[shelfIndex][slotIndex]
+    private fun WineRack.getAt(shelfIndex: Int) = this[shelfIndex]
 }
 
 
