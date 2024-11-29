f6ec7395671369202b7d236c1f5b5c43e4cdc9c6
Morgan Renou <morgan.renou@4sh.fr>
2024-11-29 09:26:01 +0100

    tp2 - step2

--- src/test/kotlin/fmt/kotlin/advanced/WineRackTest.kt
+++ src/test/kotlin/fmt/kotlin/advanced/WineRackTest.kt
@@ -6,12 +6,8 @@ import fmt.kotlin.advanced.Region.BORDEAUX
 import io.kotest.assertions.throwables.shouldThrowMessage
 import org.junit.jupiter.api.Test
 
-// tp2-step1-004
 class WineRackTest {
 
-    // tp2-step1-002
-    // hint - extension function
-    // hint - infix
     @Test
     fun `should throw error when out of capacity (shelf)`() {
         // Given
@@ -23,9 +19,6 @@ class WineRackTest {
         }
     }
 
-    // tp2-step1-002
-    // hint - extension function
-    // hint - infix
     @Test
     fun `should throw error when out of capacity (slot)`() {
         // Given
@@ -37,9 +30,6 @@ class WineRackTest {
         }
     }
 
-    // tp2-step1-002
-    // hint - extension function
-    // hint - infix
     @Test
     fun `should throw error when there is already a bottle`() {
         // Given
