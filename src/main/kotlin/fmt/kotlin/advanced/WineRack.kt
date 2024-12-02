989e2a44f73a280b8e4894b131eee7838e4aad6e
Morgan Renou <morgan.renou@4sh.fr>
2024-12-02 10:46:44 +0100

    tp1 - step1 - solution
     Conflicts:
    	src/main/kotlin/fmt/kotlin/advanced/WineRack.kt

--- src/main/kotlin/fmt/kotlin/advanced/WineRack.kt
+++ src/main/kotlin/fmt/kotlin/advanced/WineRack.kt
@@ -1,90 +1,60 @@
 package fmt.kotlin.advanced
 
-import java.lang.StringBuilder
 import java.util.*
+
+// tp1-step1-006
+// hint - top level constant
+private const val EMPTY = "empty"
 
 data class Capacity(val nbOfShelves: Int, val maxSlotByShelf: Int)
 
 data class Position(val shelfIndex: Int, val slotIndex: Int)
 
-class WineRack(val capacity: Capacity, val rackId: String = UUID.randomUUID().toString()) {
-
+data class WineRack(val capacity: Capacity, val rackId: String = UUID.randomUUID().toString()) {
     // tp1-step1-008
     // hint - scope function
-    private val bottles: MutableList<MutableList<Bottle?>> = mutableListOf()
-    init {
+    private val bottles: MutableList<MutableList<Bottle?>> = mutableListOf<MutableList<Bottle?>>().apply {
         repeat(capacity.nbOfShelves) {
-            bottles.add(arrayOfNulls<Bottle>(capacity.maxSlotByShelf).toMutableList())
+           add(arrayOfNulls<Bottle>(capacity.maxSlotByShelf).toMutableList())
         }
     }
 
     // tp1-step1-001
     // hint - check/require
     fun storeBottle(bottle: Bottle, position: Position) {
-        if (position.shelfIndex >= capacity.nbOfShelves || position.slotIndex >= capacity.maxSlotByShelf) {
-            throw IllegalArgumentException("wine rack position $position is out of capacity $capacity")
-        }
-        if (bottles[position.shelfIndex][position.slotIndex] != null) {
-            throw IllegalArgumentException("the slot at $position is not free")
-        }
+        check(position.shelfIndex < capacity.nbOfShelves && position.slotIndex < capacity.maxSlotByShelf)
+        { "wine rack position $position is out of capacity $capacity" }
+        check(bottles[position.shelfIndex][position.slotIndex] == null)
+        { "the slot at $position is not free" }
+
         bottles[position.shelfIndex][position.slotIndex] = bottle
     }
 
     // tp1-step1-002
     // hint - scope function
     // hint - body expression
-    fun takeBottle(position: Position): Bottle? {
-        val bottle = viewBottle(position)
-        bottles[position.shelfIndex][position.slotIndex] = null
-        return bottle
-    }
-
+    fun takeBottle(position: Position): Bottle? =
+        viewBottle(position).also {
+            bottles[position.shelfIndex][position.slotIndex] = null
+        }
 
     // tp1-step1-003
     // hint - body expression
-    fun viewBottle(position: Position): Bottle? {
-        return bottles[position.shelfIndex][position.slotIndex]
-    }
+    fun viewBottle(position: Position): Bottle? = bottles[position.shelfIndex][position.slotIndex]
 
     // tp1-step1-004
     // hint - property getter
     // hint - collection - aggregation
     // hint - lambda - unique parameter
     // hint - lambda - trailing
-    fun getNumberOfBottles(): Int {
-        var counter = 0
-        for (shelf in bottles) {
-            for (slot in shelf) {
-                if (slot != null) {
-                    counter++
-                }
-            }
-        }
-        return counter
-    }
+    val numberOfBottles: Int
+        get() = bottles.sumOf { it.filterNotNull().size }
 
     // tp1-step1-005
     // hint - sequence
     // hint - collection - filter
     // hint - collection - transformation
-    fun streamBottles(): Sequence<Bottle> {
-        val toReturn = mutableListOf<Bottle?>()
-        for (shelf in bottles) {
-            for (slot in shelf) {
-                toReturn.add(slot)
-            }
-        }
-        return sequence {
-            var i = 0
-            while (i < toReturn.size) {
-                val bottle = toReturn[i]
-                if (bottle != null) {
-                    yield(bottle)
-                }
-                i++
-            }
-        }
-    }
+    fun streamBottles(): Sequence<Bottle> = bottles.asSequence().flatten().filterNotNull()
 
     // tp1-step1-006
     // hint - range
@@ -94,83 +64,26 @@ class WineRack(val capacity: Capacity, val rackId: String = UUID.randomUUID().to
     // hint - collection - transformation
     // hint - top level constant
     override fun toString(): String {
-        val maxLengthBySlotIndex = mutableMapOf<Int, Int>()
-        var slotIndex = 0
-        while (slotIndex < capacity.maxSlotByShelf) {
-            var shelfIndex = 0
-            while (shelfIndex < capacity.nbOfShelves) {
-                var maxLength = 0
-                if (bottles[shelfIndex][slotIndex] != null) {
-                    maxLength = bottles[shelfIndex][slotIndex]!!.name.length + 5
-                } else {
-                    maxLength = "empty".length + 5
-                }
-                if (maxLengthBySlotIndex[slotIndex] != null) {
-                    if (maxLengthBySlotIndex[slotIndex]!! < maxLength) {
-                        maxLengthBySlotIndex[slotIndex] = maxLength
-                    }
-                } else {
-                    maxLengthBySlotIndex[slotIndex] = maxLength
-                }
-                shelfIndex++
+        val maxLengthBySlotIndex: Map<Int, Int> = (0 until capacity.maxSlotByShelf)
+            .associateWith { slotIndex ->
+                (0 until capacity.nbOfShelves)
+                    .maxOf { shelfIndex -> (bottles[shelfIndex][slotIndex]?.name ?: EMPTY).length + 5 }
             }
-            slotIndex++
-        }
-
-        val stringBuilder = StringBuilder()
-        var shelfIndex = 0
-        while (shelfIndex < capacity.nbOfShelves) {
-            var slotIndex = 0
-            while (slotIndex < capacity.maxSlotByShelf) {
-                if (slotIndex > 0) {
-                    stringBuilder.append(" / ")
+        return bottles.joinToString("\n") { shelf ->
+            shelf
+                .mapIndexed { index, bottle ->
+                    (bottle?.let { "${it.name} ${it.year}" } ?: EMPTY)
+                        .padEnd(maxLengthBySlotIndex.getValue(index))
                 }
-                var description = ""
-                if (bottles[shelfIndex][slotIndex] != null) {
-                    description = bottles[shelfIndex][slotIndex]!!.name + " " + bottles[shelfIndex][slotIndex]!!.year
-                } else {
-                    description = "empty"
-                }
-                stringBuilder.append(description.padEnd(maxLengthBySlotIndex[slotIndex]!!))
-                slotIndex++
-            }
-            stringBuilder.append("\n")
-            shelfIndex++
-        }
-        stringBuilder.append("\n")
-
-        return stringBuilder.toString()
-    }
-
-    // tp1-step1-007
-    // hint - class
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as WineRack
-
-        if (capacity != other.capacity) return false
-        if (rackId != other.rackId) return false
-        if (bottles != other.bottles) return false
-
-        return true
-    }
-
-    // tp1-step1-007
-    // hint - class
-    override fun hashCode(): Int {
-        var result = capacity.hashCode()
-        result = 31 * result + rackId.hashCode()
-        result = 31 * result + bottles.hashCode()
-        return result
+                .joinToString(" / ")
+        } + "\n"
     }
 
     fun at(shelfIndex: Int, slotIndex: Int): Bottle? {
         return bottles[shelfIndex][slotIndex]
     }
 
-    fun at( shelfIndex: Int): MutableList<Bottle?> {
+    fun at(shelfIndex: Int): MutableList<Bottle?> {
         return bottles[shelfIndex]
     }
 }
