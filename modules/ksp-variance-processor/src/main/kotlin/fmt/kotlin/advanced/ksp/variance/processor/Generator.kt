d96b216fd276989576ba39427faba6044d2adc28
Morgan Renou <morgan.renou@4sh.fr>
2024-12-04 23:24:35 +0100

    tp8 - step2

--- modules/ksp-variance-processor/src/main/kotlin/fmt/kotlin/advanced/ksp/variance/processor/Generator.kt
+++ modules/ksp-variance-processor/src/main/kotlin/fmt/kotlin/advanced/ksp/variance/processor/Generator.kt
@@ -1,6 +1,9 @@
 package fmt.kotlin.advanced.ksp.variance.processor
 
 
+import com.google.devtools.ksp.getDeclaredFunctions
+import com.google.devtools.ksp.getDeclaredProperties
+import com.google.devtools.ksp.getVisibility
 import com.google.devtools.ksp.processing.CodeGenerator
 import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.symbol.KSClassDeclaration
@@ -10,6 +13,12 @@ import com.squareup.kotlinpoet.TypeSpec
 import com.squareup.kotlinpoet.TypeVariableName
 import com.squareup.kotlinpoet.ksp.addOriginatingKSFile
 import com.squareup.kotlinpoet.ksp.writeTo
+import com.google.devtools.ksp.symbol.*
+import com.squareup.kotlinpoet.*
+
+import com.squareup.kotlinpoet.ksp.*
+
+private val ignoredFunctions = setOf("<init>", "toString")
 
 class Generator(private val codeGenerator: CodeGenerator, private val logger: KSPLogger) {
 
@@ -41,8 +50,9 @@ class Generator(private val codeGenerator: CodeGenerator, private val logger: KS
         classType: KSClassDeclaration,
         readable: Boolean,
     ): TypeSpec {
-        val interfaceName = readable.takeIf { it }?.let { "TmpReadable$simpleName" } ?: "TmpWriteable$simpleName"
+        val interfaceName = readable.takeIf { it }?.let { "Readable$simpleName" } ?: "Writeable$simpleName"
         logger.warn("generate $interfaceName")
+        val typeParamResolver = classType.typeParameters.toTypeParameterResolver()
         return TypeSpec.interfaceBuilder(interfaceName)
             .addTypeVariables(classType.typeParameters.map { typeParam ->
                 TypeVariableName(
@@ -50,6 +60,56 @@ class Generator(private val codeGenerator: CodeGenerator, private val logger: KS
                     variance = readable.takeIf { it }?.let { KModifier.OUT } ?: KModifier.IN)
             })
             .addOriginatingKSFile(classType.containingFile!!)
+            .addFunctions(generateFunctions(classType, typeParamResolver, readable))
+            .addProperties(generateProperties(classType, typeParamResolver))
             .build()
     }
+
+
+    private fun generateFunctions(
+        classType: KSClassDeclaration,
+        typeParamResolver: TypeParameterResolver,
+        readable: Boolean = true
+    ): List<FunSpec> {
+        val classTypeArgumentName = classType.typeParameters.map { it.name.asString() }.toSet()
+        return classType.getDeclaredFunctions()
+            //
+            .toList()
+            .mapNotNull { func ->
+                generateFunction(func, typeParamResolver)
+            }
+    }
+
+    private fun generateFunction(
+        function: KSFunctionDeclaration,
+        typeParamResolver: TypeParameterResolver
+    ): FunSpec? =
+        function.returnType?.let { returnType ->
+            logger.warn("generate function ${function.simpleName.asString()}")
+
+
+
+        }
+
+    private fun generateParam(
+        param: KSValueParameter,
+        typeParamResolver: TypeParameterResolver
+    ): ParameterSpec? = param.name?.let {
+
+    }
+
+    private fun generateProperties(
+        classType: KSClassDeclaration,
+        typeParamResolver: TypeParameterResolver
+    ): List<PropertySpec> = classType.getDeclaredProperties()
+        .filter { prop -> prop.getVisibility() == Visibility.PUBLIC }
+        .toList()
+        .map { prop ->
+            logger.warn("generate property ${prop.simpleName.asString()}")
+
+
+        }
+
+    private fun KSTypeReference.getTypeNames() =
+        resolve().arguments.mapNotNull { it.type?.toString() } + resolve().declaration.simpleName.asString()
 }
