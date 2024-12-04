c6b64b80d5ab09feb9c74793b13926912fad9918
Xavier Hanin <xavier.hanin@4sh.fr>
2024-12-04 15:46:33 +0100

    coroutines - tp2 - step 3 - solution

--- src/main/kotlin/fmt/kotlin/advanced/Tp2.kt
+++ src/main/kotlin/fmt/kotlin/advanced/Tp2.kt
@@ -5,6 +5,9 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.flow.take
+import kotlin.coroutines.AbstractCoroutineContextElement
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
 import kotlin.test.Test
 import kotlin.time.measureTimedValue
 
@@ -16,9 +19,30 @@ class Tp2 {
         }
     }
 
-    suspend fun averageLag(clockFlow: Flow<Tick>, simulations: Int) = coroutineScope {
-        (1..simulations).map {
-            async { clockFlow.take(20).last().lagInMsPerSecond }
+    object NoOpResultsCollector : SimulationResultsCollector {
+        override suspend fun collectResult(result: SimulationResult) {
+        }
+    }
+
+    class SimuClockContextElement(val index: Int) : AbstractCoroutineContextElement(Key) {
+        companion object Key : CoroutineContext.Key<SimuClockContextElement>
+    }
+
+    class ConsoleResultsCollector : SimulationResultsCollector {
+        override suspend fun collectResult(result: SimulationResult) {
+            delay(5)
+            println("[${coroutineContext[SimuClockContextElement]?.index}] ${result.lagInMsPerSecond}")
+        }
+    }
+
+    suspend fun averageLag(clockFlow: Flow<Tick>, simulations: Int, collector: SimulationResultsCollector = NoOpResultsCollector) = coroutineScope {
+        (1..simulations).map { index ->
+            async(SimuClockContextElement(index)) {
+                clockFlow.take(20).last().lagInMsPerSecond
+                    .also {
+                        collector.collectResult(SimulationResult(it))
+                    }
+            }
         }.awaitAll()
             .average()
 
@@ -56,6 +80,15 @@ class Tp2 {
         runBlocking(Dispatchers.Default) {
             measureTimedValue {
                 averageLag(clockFlow { SimuClock.newClock() }, 300)
+            }.also { println(it) }
+        }
+    }
+
+    @Test
+    fun ex3() {
+        runBlocking(Dispatchers.Default) {
+            measureTimedValue {
+                averageLag(clockFlow { SimuClock.newClock() }, 5, ConsoleResultsCollector())
             }.also { println(it) }
         }
     }
