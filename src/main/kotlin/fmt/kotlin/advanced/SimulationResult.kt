7eddf5a15f93787dce0a6fddedf334f68de75698
Xavier Hanin <xavier.hanin@4sh.fr>
2024-12-04 18:28:52 +0100

    coroutines - tp3

--- src/main/kotlin/fmt/kotlin/advanced/SimulationResult.kt
+++ src/main/kotlin/fmt/kotlin/advanced/SimulationResult.kt
@@ -1,16 +1,77 @@
 package fmt.kotlin.advanced
 
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.delay
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.coroutines.cancellation.CancellationException
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
 //
 // !!!! NE PAS MODIFIER !!!!
 //
-data class SimulationResult(val lagPerSecond: Duration?)
+data class SimulationResult(
+    val lagPerSecond: Duration?,
+    val iterations: Int = -1,
+    val cancellationCause: Exception? = null
+)
 
-//
-// !!!! NE PAS MODIFIER !!!!
-//
-
-interface SimulationResultsCollector {
+fun interface SimulationResultsCollector {
     suspend fun collectResult(result: SimulationResult)
+}
+
+class SimulationsCountStats : SimulationResultsCollector {
+    val capturedCount = AtomicInteger()
+    val cancelledCount = AtomicInteger()
+    val timeoutCount = AtomicInteger()
+    val errorCount = AtomicInteger()
+    val collectedCount = AtomicInteger()
+
+    override suspend fun collectResult(result: SimulationResult) {
+        capturedCount.incrementAndGet()
+        when (result.cancellationCause) {
+            is TimeoutCancellationException -> {
+                timeoutCount.incrementAndGet()
+            }
+
+            is CancellationException -> {
+                cancelledCount.incrementAndGet()
+            }
+
+            is Exception -> {
+                errorCount.incrementAndGet()
+            }
+        }
+        if (result.iterations > 0) {
+            collectedCount.addAndGet(result.iterations)
+        }
+    }
+
+    fun printStats() {
+        val captured = capturedCount.get()
+        val cancelled = cancelledCount.get()
+        val timeout = timeoutCount.get()
+        val collected = collectedCount.get()
+
+        println("STATS • captured: $captured • cancelled: $cancelled • timeout: $timeout • collected: $collected")
+    }
+}
+
+class AvgLagStatsCollector : SimulationResultsCollector {
+    private var count: Int = 0
+    private var totalLagPerSecond: Duration = 0.milliseconds
+
+    val simulationCount get() = count
+    val avgLagPerSecond: Duration get() = totalLagPerSecond / count.toDouble()
+
+    // not thread safe
+    override suspend fun collectResult(result: SimulationResult) {
+        delay(5)
+        count++
+        result.lagPerSecond?.also { totalLagPerSecond += it }
+    }
+
+    fun printStats() {
+        println("Retard par seconde moyen : $avgLagPerSecond pendant $simulationCount simulations")
+    }
 }
