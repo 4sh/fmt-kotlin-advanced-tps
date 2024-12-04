fb165b7c60d126c95f4831327f7d1abe4303948e
Xavier Hanin <xavier.hanin@4sh.fr>
2024-12-04 15:32:37 +0100

    coroutines - tp2 - step 2 - solution

--- src/test/kotlin/fmt/kotlin/advanced/Tp2.kt
+++ src/test/kotlin/fmt/kotlin/advanced/Tp2.kt
@@ -1,13 +1,10 @@
 package fmt.kotlin.advanced
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.flow.take
-import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import kotlin.time.Duration
 import kotlin.time.measureTimedValue
@@ -24,7 +21,7 @@ class Tp2 {
      * Exécute [simulations] instances [clockFlow] en parallèle, et renvoie la moyenne de leur [Tick.lagPerSecond].
      */
     suspend fun averageLag(clockFlow: Flow<Tick>, simulations: Int): Duration = coroutineScope {
-        (1..simulations).map {
+        (1..simulations).map { index ->
             async {
                 clockFlow.take(20).last().lagPerSecond
             }
@@ -36,6 +33,33 @@ class Tp2 {
     fun ex1() = runBlocking {
         val (averageLag, totalTime) = measureTimedValue {
             averageLag(clockFlow { BlockingSimuClock() }, 5)
+        }
+
+        println("Retard par seconde : $averageLag pendant $totalTime")
+    }
+
+    @Test
+    fun ex2_1() = runBlocking(Dispatchers.Default) {
+        val (averageLag, totalTime) = measureTimedValue {
+            averageLag(clockFlow { BlockingSimuClock() }, 5)
+        }
+
+        println("Retard par seconde : $averageLag pendant $totalTime")
+    }
+
+    @Test
+    fun ex2_2() = runBlocking(Dispatchers.Default) {
+        val (averageLag, totalTime) = measureTimedValue {
+            averageLag(clockFlow { BlockingSimuClock() }, 300)
+        }
+
+        println("Retard par seconde : $averageLag pendant $totalTime")
+    }
+
+    @Test
+    fun ex2_3() = runBlocking(Dispatchers.Default) {
+        val (averageLag, totalTime) = measureTimedValue {
+            averageLag(clockFlow { SimuClock.newClock() }, 300)
         }
 
         println("Retard par seconde : $averageLag pendant $totalTime")
