353d3d14eaa17edc001d23422add1530bef5984d
Morgan Renou <morgan.renou@4sh.fr>
2024-12-04 23:11:12 +0100

    tp8 - step1 - partial

---
+++ modules/annotation-variance/src/main/kotlin/fmt/kotlin/advanced/annotation/variance/VarianceInterface.kt
@@ -0,0 +1,4 @@
+package fmt.kotlin.advanced.annotation.variance
+
+@Target(AnnotationTarget.CLASS)
+annotation class VarianceInterface()
--- modules/ksp-variance-processor/src/main/kotlin/fmt/kotlin/advanced/ksp/variance/processor/Processor.kt
+++ modules/ksp-variance-processor/src/main/kotlin/fmt/kotlin/advanced/ksp/variance/processor/Processor.kt
@@ -5,11 +5,16 @@ import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.symbol.KSAnnotated
+import com.google.devtools.ksp.validate
+import fmt.kotlin.advanced.annotation.variance.VarianceInterface
 
 class Processor(codeGenerator: CodeGenerator, private val logger: KSPLogger) : SymbolProcessor {
 
-    override fun process(resolver: Resolver): List<KSAnnotated> {
-        return listOf()
-    }
+    private val visitor = Visitor(codeGenerator, logger)
 
+    override fun process(resolver: Resolver): List<KSAnnotated> {
+        logger.warn("[VARIANCE INTERFACE] Process sources...")
+
+        // get symbol by annotation and process it with visitor only if invalid
+    }
 }
