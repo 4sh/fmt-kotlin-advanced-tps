1bc0f61dd73f925b13017b75a00fa24f04213b20
Xavier Hanin <xavier.hanin@4sh.fr>
2024-12-04 15:28:18 +0100

    coroutines - tp2 - step 1 - solution

--- src/test/kotlin/fmt/kotlin/advanced/Tp2.kt
+++ src/test/kotlin/fmt/kotlin/advanced/Tp2.kt
@@ -1,21 +1,43 @@
 package fmt.kotlin.advanced
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import kotlin.time.Duration
+import kotlin.time.measureTimedValue
 
 class Tp2 {
-    fun clockFlow(simuClock: () -> SimuClock): Flow<Tick> =
-        TODO()
+    fun clockFlow(simuClock: () -> SimuClock) = flow {
+        val clock = simuClock()
+        while (true) {
+            emit(clock.nextTick())
+        }
+    }
 
     /**
      * Exécute [simulations] instances [clockFlow] en parallèle, et renvoie la moyenne de leur [Tick.lagPerSecond].
      */
-    suspend fun averageLag(clockFlow: Flow<Tick>, simulations: Int): Duration =
-        TODO()
+    suspend fun averageLag(clockFlow: Flow<Tick>, simulations: Int): Duration = coroutineScope {
+        (1..simulations).map {
+            async {
+                clockFlow.take(20).last().lagPerSecond
+            }
+        }.awaitAll()
+            .average()
+    }
 
     @Test
-    fun ex1() {
+    fun ex1() = runBlocking {
+        val (averageLag, totalTime) = measureTimedValue {
+            averageLag(clockFlow { BlockingSimuClock() }, 5)
+        }
 
+        println("Retard par seconde : $averageLag pendant $totalTime")
     }
 }
