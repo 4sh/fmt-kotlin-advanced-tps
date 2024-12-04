5868ed9890814fd239a245fdce33cfa196e02645
Xavier Hanin <xavier.hanin@4sh.fr>
2024-12-04 14:38:35 +0100

    coroutines - tp1 - step 3 - solution

--- src/test/kotlin/fmt/kotlin/advanced/Tp1.kt
+++ src/test/kotlin/fmt/kotlin/advanced/Tp1.kt
@@ -1,11 +1,14 @@
 package fmt.kotlin.advanced
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
+import kotlin.time.Duration.Companion.seconds
 
 class Tp1 {
     @Test
@@ -30,5 +33,29 @@ class Tp1 {
             .onEach { println(it) }
             .last()
         println("Retard par seconde: ${lastTick.lagPerSecond}")
+    }
+
+    @Test
+    fun ex3() {
+        val clockFlow = flow {
+            val simuClock = StdSimuClock()
+            while (true) {
+                emit(simuClock.nextTick())
+            }
+        }
+
+        suspend fun simulate(name: String) {
+            val lastTick = clockFlow
+                .take(20)
+                .onEach { println("[$name] $it") }
+                .last()
+            println("[$name] Retard par seconde: ${lastTick.lagPerSecond}")
+        }
+
+        runBlocking {
+            launch { simulate("A") }
+            delay(1.seconds)
+            launch { simulate("B") }
+        }
     }
 }
