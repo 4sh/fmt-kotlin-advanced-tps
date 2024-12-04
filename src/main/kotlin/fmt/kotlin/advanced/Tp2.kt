df095763904f37f9b21a98c327d001dde6e7d2e6
Xavier Hanin <xavier.hanin@4sh.fr>
2024-12-04 15:28:18 +0100

    coroutines - tp2 - step 1 - solution

--- src/main/kotlin/fmt/kotlin/advanced/Tp2.kt
+++ src/main/kotlin/fmt/kotlin/advanced/Tp2.kt
@@ -1,22 +1,39 @@
 package fmt.kotlin.advanced
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.flow.take
-import kotlin.system.measureTimeMillis
+import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 import kotlin.time.measureTimedValue
 
 class Tp2 {
-    fun clockFlow(simuClock: () -> SimuClock): Flow<Tick> =
+    fun clockFlow(simuClock: () -> SimuClock) = flow {
+        val clock = simuClock()
+        while (true) {
+            emit(clock.nextTick())
+        }
+    }
 
-    suspend fun averageLag(clockFlow: Flow<Tick>, simulations: Int): Double =
+    suspend fun averageLag(clockFlow: Flow<Tick>, simulations: Int) = coroutineScope {
+        (1..simulations).map {
+            async { clockFlow.take(20).last().lagInMsPerSecond }
+        }.awaitAll()
+            .average()
+
+    }
 
     @Test
     fun ex1() {
-
+        runBlocking {
+            measureTimedValue {
+                averageLag(clockFlow { BlockingSimuClock() }, 5)
+            }.also { println(it) }
+        }
     }
 
 }
