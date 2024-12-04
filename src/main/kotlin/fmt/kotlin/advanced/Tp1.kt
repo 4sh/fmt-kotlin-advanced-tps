1c629b9b8225df4800827d612146cdf2ec26443f
Xavier Hanin <xavier.hanin@4sh.fr>
2024-12-04 14:32:30 +0100

    coroutines - tp1 - step 2 - solution

--- src/main/kotlin/fmt/kotlin/advanced/Tp1.kt
+++ src/main/kotlin/fmt/kotlin/advanced/Tp1.kt
@@ -1,5 +1,8 @@
 package fmt.kotlin.advanced
 
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 
@@ -11,6 +14,25 @@ class Tp1 {
             repeat(20) {
                 println(simuClock.nextTick())
             }
+        }
+    }
+
+    @Test
+    fun ex2() {
+        runBlocking {
+            val clockFlow = flow {
+                val simuClock = StdSimuClock()
+                while(true) {
+                    emit(simuClock.nextTick())
+                }
+            }
+
+            var lastTick: Tick? = null
+            clockFlow
+                .take(20)
+                .onEach { lastTick = it }
+                .collect { println(it) }
+            println("avg: ${lastTick?.lagInMsPerSecond}")
         }
     }
 }
