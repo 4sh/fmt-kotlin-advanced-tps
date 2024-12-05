d6d9957177cac1930db97aeac401861d087e95d4
Xavier Hanin <xavier.hanin@4sh.fr>
2024-12-05 15:08:59 +0100

    coroutines - tp4 - step 1 - solution

--- src/main/kotlin/fmt/kotlin/advanced/Tp4.kt
+++ src/main/kotlin/fmt/kotlin/advanced/Tp4.kt
@@ -1,8 +1,8 @@
 package fmt.kotlin.advanced
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
 import kotlin.test.Test
 
 class Tp4 {
@@ -24,7 +24,8 @@ class Tp4 {
                 batchCount = 100,
                 simulationPerBatch = 10,
                 simulator = simulator,
-                clockFlow = clockFlow { SimuClock.newClock() })
+                clockFlow = clockFlow { SimuClock.newClock() }
+            )
                 .simulate {
                     collector.collectResult(it)
                     avgCollector.collectResult(it)
@@ -33,4 +34,57 @@ class Tp4 {
             avgCollector.printStats()
         }
     }
+
+    class FlowBasedSimulationManager(
+        val batchCount: Int,
+        val simulationPerBatch: Int,
+        val simulator: Simulator,
+        val clockFlow: Flow<Tick>
+    ) : SimulationManager {
+
+        override suspend fun simulate(collector: SimulationResultsCollector) {
+            coroutineScope {
+                (1..batchCount)
+                    .asFlow()
+                    .map {
+                        BatchSimulator(it, simulationPerBatch, simulator)
+                            .withThreshold()
+                    }
+                    .map { batch ->
+                        channelFlow {
+                            batch.simulate(clockFlow) {
+                                channel.send(it)
+                            }
+                        }
+                    }
+                    .flattenMerge()
+                    .collect {
+                        collector.collectResult(it)
+                    }
+            }
+        }
+    }
+
+    @Test
+    fun ex1() {
+        val simulator = StdSimulator(iterations = 20).withTimeout()
+        val collector = SimulationsCountStats()
+        val avgCollector = AvgLagStatsCollector()
+
+        runBlocking(Dispatchers.Default) {
+            FlowBasedSimulationManager(
+                batchCount = 100,
+                simulationPerBatch = 10,
+                simulator = simulator,
+                clockFlow = clockFlow { SimuClock.newClock() }
+            )
+                .simulate {
+                    collector.collectResult(it)
+                    avgCollector.collectResult(it)
+                }
+            collector.printStats()
+            avgCollector.printStats()
+        }
+    }
+
 }
