fe5d110ff4ad04fabd2a6ba847b9c55c60e4bd04
Xavier Hanin <xavier.hanin@4sh.fr>
2024-12-05 18:54:38 +0100

    coroutines - tp4 - step 2 - solution

--- src/main/kotlin/fmt/kotlin/advanced/Tp4.kt
+++ src/main/kotlin/fmt/kotlin/advanced/Tp4.kt
@@ -1,10 +1,9 @@
 package fmt.kotlin.advanced
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
+import kotlin.time.Duration.Companion.milliseconds
 
 class Tp4 {
     fun clockFlow(simuClock: () -> SimuClock) = flow {
@@ -83,6 +82,50 @@ class Tp4 {
                     collector.collectResult(it)
                     avgCollector.collectResult(it)
                 }
+            collector.printStats()
+            avgCollector.printStats()
+        }
+    }
+
+    class ObservableAvgLagStatsCollector(val avgCollector: AvgLagStatsCollector) : SimulationResultsCollector {
+        private val avgMutableFlow = MutableStateFlow<Double>(0.0)
+        val avgFlow: StateFlow<Double> = avgMutableFlow.asStateFlow()
+        val avgLagInMsPerSecond: Double get() = avgFlow.value
+
+        override suspend fun collectResult(result: SimulationResult) {
+            avgCollector.collectResult(result)
+            avgMutableFlow.emit(avgCollector.avgLagInMsPerSecond)
+        }
+
+        fun printStats() {
+            avgCollector.printStats()
+        }
+    }
+
+    @Test
+    fun ex2() {
+        val simulator = StdSimulator(iterations = 20).withTimeout()
+        val collector = SimulationsCountStats()
+        val avgCollector = ObservableAvgLagStatsCollector(AvgLagStatsCollector(1.milliseconds))
+
+        runBlocking(Dispatchers.Default) {
+            val printAvgJob = launch(CoroutineName("print_avg_job")) {
+                avgCollector.avgFlow.buffer().drop(1).conflate().collect {
+                    println("AVG: $it")
+                    delay(1000)
+                }
+            }
+            FlowBasedSimulationManager(
+                batchCount = 100,
+                simulationPerBatch = 50,
+                simulator = simulator,
+                clockFlow = clockFlow { SimuClock.newClock() }
+            )
+                .simulate {
+                    collector.collectResult(it)
+                    avgCollector.collectResult(it)
+                }
+            printAvgJob.cancel()
             collector.printStats()
             avgCollector.printStats()
         }
